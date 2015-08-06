e03510bde2d3a8750b439b419235e342087982d9
Ravi Gaddipati <ravigaddipati@gmail.com>
2015-08-06 19:53:30 -0400

    basic framework

--- main.cpp
+++ main.cpp
@@ -2,50 +2,77 @@
 // Created by gaddra on 8/6/15.
 //
 
-#include <iostream>
-#include "gssw/src/gssw.h"
 #include "main.h"
 
 using std::string;
+using std::cout;
+using std::endl;
+using std::cerr;
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
     /** Scores **/
     int32_t match = 2, mismatch = 2, gap_open = 3, gap_extension = 1;
+    string VCF, REF;
 
-    char *ref = "GCCCGCGTAGCAGCATA";
-    char *query = "CATAGCCAACG";
+    GetOpt::GetOpt_pp args(argc, argv);
+    if(args >> GetOpt::OptionPresent('h', "help"))
+    {
+        cout << "VMatch options:" << endl;
+        cout << "-v\t--vcf           VCF file, uncompressed" << endl;
+        cout << "-r\t--ref           reference FASTA" << endl;
+        cout << "-m\t--match         Match score, default  " << match << endl;
+        cout << "-n\t--mismatch      Mismatch score, default " << mismatch << endl;
+        cout << "-o\t--gap_open      Gap opening score, default " << gap_open << endl;
+        cout << "-e\t--gap_extend    Gap extend score, default " << gap_extension << endl;
+        exit(0);
+    }
+    if (!(args >> GetOpt::Option('v', "vcf", VCF))
+        || !(args >> GetOpt::Option('r', "ref", REF)))
+    {
+        cerr << "VCF and/or ref file not specified, use -v and -r." << endl;
+        exit(1);
+    }
+    args >> GetOpt::Option('m', "match", match)
+         >> GetOpt::Option('n', "mismatch", mismatch)
+         >> GetOpt::Option('o', "gap_open", gap_open)
+         >> GetOpt::Option('e', "gap_extend", gap_extension);
+
+    char *ref0 = "GGGCG", *ref1 = "TAAAAA", *ref2 = "ACGTGCA";
+    char *query = "ACGT";
 
     int8_t *nt_table = gssw_create_nt_table(); // Nucleotide -> Num
     int8_t *mat = gssw_create_score_matrix(match, mismatch);
 
-    gssw_node* nodes[2];
-    nodes[0] = gssw_node_create(ref, 1, ref, nt_table, mat);
-    nodes[1] = gssw_node_create(ref, 2, ref, nt_table, mat);
+    gssw_node* nodes[3];
+    nodes[0] = gssw_node_create(ref0, 0, ref0, nt_table, mat);
+    nodes[1] = gssw_node_create(ref1, 1, ref1, nt_table, mat);
+    nodes[2] = gssw_node_create(ref2, 2, ref1, nt_table, mat);
 
     gssw_nodes_add_edge(nodes[0], nodes[1]);
+    gssw_nodes_add_edge(nodes[1], nodes[2]);
 
-    gssw_graph* graph = gssw_graph_create(2);
+    gssw_graph* graph = gssw_graph_create(3);
     gssw_graph_add_node(graph, nodes[0]);
     gssw_graph_add_node(graph, nodes[1]);
+    gssw_graph_add_node(graph, nodes[2]);
 
     gssw_graph_fill(graph, query, nt_table, mat, gap_open, gap_extension, 15, 2);
 
-    gssw_graph_mapping* gm = gssw_graph_trace_back (graph,
-                                                    query,
-                                                    strlen(query),
-                                                    match,
-                                                    mismatch,
-                                                    gap_open,
-                                                    gap_extension);
+    printNode(graph->nodes[0]);
+    printNode(graph->nodes[1]);
+    printNode(graph->nodes[2]);
+    std::cout << graph->max_node->id;
 
-    gssw_print_graph_mapping(gm);
-    gssw_graph_mapping_destroy(gm);
-    // note that nodes which are referred to in this graph are destroyed as well
+
     gssw_graph_destroy(graph);
-
-    free(nt_table);
-    free(mat);
+    delete[] nt_table;
+    delete[] mat;
 
     return(0);
+}
+void printNode(gssw_node *node) {
+    using namespace std;
+    cout << "optimal score: " << node->alignment->score1 << " end: " << node->alignment->ref_end1 << endl;
+    cout << "suboptimal score: " << node->alignment->score2 << " end: " << node->alignment->ref_end2 << endl << endl;
 }
--- main.h
+++ main.h
@@ -5,4 +5,10 @@
 #ifndef PROJECT_MAIN_H
 #define PROJECT_MAIN_H
 
+#include <iostream>
+#include "gssw/src/gssw.h"
+#include "getopt_pp.h"
+
+void printNode(gssw_node *node);
+
 #endif //PROJECT_MAIN_H
