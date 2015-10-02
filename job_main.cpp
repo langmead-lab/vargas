670b817477ee67fbb7ba9c19cdff8ea6141b0930
Ravi Gaddipati <ravigaddipati@gmail.com>
2015-10-02 13:44:49 -0400

    sub mains to int type, blank job main

---
+++ job_main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gaddra on 10/2/15.
+//
+
+#include "job_main.h"
---
+++ job_main.h
@@ -0,0 +1,8 @@
+//
+// Created by gaddra on 10/2/15.
+//
+
+#ifndef VMATCH_JOB_MAIN_H
+#define VMATCH_JOB_MAIN_H
+
+#endif //VMATCH_JOB_MAIN_H
