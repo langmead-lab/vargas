d6ada966638c7832b20910e1b8b870aa26829e50
dnbaker <dnb@cs.jhu.edu>
2019-03-28 14:54:29 -0400

    Forgotten include.

---
+++ include/threadpool.h
@@ -0,0 +1,17 @@
+#pragma once
+#define KTHREAD_STORAGE static inline
+#include "./kthread.h"
+#include "./kthread.c"
+
+namespace rg {
+struct ForPool {                                                                                        
+    void *fp_;                                                                                          
+    ForPool(int nthreads): fp_(kt_forpool_init(nthreads)) {}                                            
+    void forpool(void (*func)(void*,long,int), void *data, long n) {                                    
+        kt_forpool(fp_, func, data, n);                                                                 
+    }                                                                                                   
+    ~ForPool() {                                                                                        
+        kt_forpool_destroy(fp_);                                                                        
+    }                                                                                                   
+};
+}
