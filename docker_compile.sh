ea7627c16b851d9f9052f7d04c0076e606c51e2a
Charlotte Darby <cdarby@jhu.edu>
2020-06-05 13:57:04 -0400

    Update docker_compile.sh

--- docker_compile.sh
+++ docker_compile.sh
@@ -4,16 +4,20 @@ cd htslib
 autoconf && autoheader && ./configure && make -j4
 cd ..
 
+
 mkdir build-sse && cd build-sse
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc .. && make -j4
+mv build/vargas vargas-sse
 cd ..
 
 
 mkdir build-avx2 && cd build-avx2
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_AVX2_GCC=ON -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc .. && make -j4
+mv build/vargas vargas-avx2
 cd ..
 
 
 mkdir build-avx512bw && cd build-avx512bw
 cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_AVX512BW_GCC=ON -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc .. && make -j4
+mv build/vargas vargas-avx512bw
 cd ..
